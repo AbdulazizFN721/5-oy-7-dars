b0770145ec2aa4f0a976a50d9a5da060c806ee6c
AbdulazizFN721 <roroc95388@artgulin.com>
2024-05-10 14:33:17 +0300

    Album xit 2023

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+    {
+      name: "Muhammadziyo",
+      artist: "Labidan",
+      img: "img1",
+      audio: "music1"
+    },
+    {
+      name: "Shoxrux Ergashev",
+      artist: "Gulim ",
+      img: "img2",
+      audio: "music2"
+    },
+    {
+      name: "Muhammadziyo",
+      artist: "Malikam",
+      img: "img3",
+      audio: "music3"
+    },
+    {
+      name: "Abror Dostov",
+      artist: "Jamalak",
+      img: "img4",
+      audio: "music4"
+    }
+  ]
